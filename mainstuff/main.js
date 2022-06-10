f9dab68c5bdd66df95f7ec72abd6a174f0a06b29
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-06-10 19:43:31 -0400

    coded banner next is the level

--- mainstuff/main.js
+++ mainstuff/main.js
@@ -1,43 +1,33 @@
-window.onload = function() {
-    var rr = 0;
-    var name1 = "N/A";
-    const apiUrl = "https://api.henrikdev.xyz/valorant/v1/";
-    var rrtext = document.querySelector(".rr");
-    var nametext = document.querySelector('.name');
-    const searchbox = document.querySelector(".searchbox");
-    const form = document.querySelector(".form_");
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault();
+window.onload = function () {
+  var rr = 0;
+  var name1 = "N/A";
+  const apiUrl = "https://api.henrikdev.xyz/valorant/v1/";
+  var rrtext = document.querySelector(".rr");
+  var nametext = document.querySelector(".name");
+  const searchbox = document.querySelector(".searchbox");
+  const form = document.querySelector(".form_");
+  const banner = document.querySelector(".banner")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const tag = searchbox.value.split("#");
+    async function doWork() {
         const tag = searchbox.value.split("#");
-    //     fetch(`${apiUrl}mmr/na/${tag[0]}/${tag[1]}`)
-    //     .then(req => req.json())
-    //     .then(x => 
-    //         console.log(x)
-    //     //     {
-    //     // rrtext.innerHTML = (x.rr);
-    //     // nametext.innerHTML = (x.name)}
-    //     )
-    //     .catch(error)(
-    //         console.log("ERROR")
-    //  );
-    async function doWork(){
+      try {
         const response = await fetch(`${apiUrl}mmr/na/${tag[0]}/${tag[1]}`);
         const json = await response.json();
-        console.log(json)
-        console.log(json.data.ranking_in_tier)
+        console.log(json.data.name);
+        console.log(json.data.ranking_in_tier);
         rrtext.innerHTML = json.data.ranking_in_tier + "/100";
         nametext.innerHTML = json.data.name;
+        const r2 = await fetch(`${apiUrl}account/${tag[0]}/${tag[1]}`);
+        const rjson = await r2.json();
+        console.log(rjson);
+        console.log(rjson.data.card.large);
+        banner.style.background =  `url("${rjson.data.card.large}")`;
+      } catch {
+        
+      }
     }
     doWork();
-    })
-    
-
-
-
-
-
-
-
-
+  });
 };
-
