d25e6688a3301fc9e74c6cce340141a8f598f0a4
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-06-11 18:25:30 -0400

    created the ranking logos and changed labels

--- mainstuff/main.js
+++ mainstuff/main.js
@@ -8,6 +8,7 @@ window.onload = function () {
   const form = document.querySelector(".form_");
   const banner = document.querySelector(".banner");
   const level = document.querySelector(".level");
+  const rank = document.querySelector(".rank");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const tag = searchbox.value.split("#");
@@ -19,15 +20,17 @@ window.onload = function () {
         const json = await response.json();
         console.log(json.data.name);
         console.log(json.data.ranking_in_tier);
-        rrtext.innerHTML = json.data.ranking_in_tier + "/100"; //mmr
+        rrtext.innerHTML = "RR = " + json.data.ranking_in_tier; //mmr
         nametext.innerHTML = json.data.name; //name
+        let tier = json.data.currenttierpatched.replace(" ", "%20");
+        rank.innerHTML = `<img src="Val_icons/${tier}.png" alt="" class="rankpic">`;
         // for the banner and account level
         const r2 = await fetch(`${apiUrl}account/${tag[0]}/${tag[1]}`); //fetch the account stats
         const rjson = await r2.json();
         console.log(rjson);
         console.log(rjson.data.card.large);
         banner.style.background = `url("${rjson.data.card.large}")`; //banner
-        level.innerHTML = rjson.data.account_level; //acc level
+        level.innerHTML = "Level = " + rjson.data.account_level; //acc level
       } catch {}
     }
     doWork();
