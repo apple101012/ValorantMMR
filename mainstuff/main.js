bc565f9802f522cbe5011c5508ae2953e3507827
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-06-11 14:02:54 -0400

    search bar and stuff is done next is ranking (G2)

--- mainstuff/main.js
+++ mainstuff/main.js
@@ -6,27 +6,29 @@ window.onload = function () {
   var nametext = document.querySelector(".name");
   const searchbox = document.querySelector(".searchbox");
   const form = document.querySelector(".form_");
-  const banner = document.querySelector(".banner")
+  const banner = document.querySelector(".banner");
+  const level = document.querySelector(".level");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const tag = searchbox.value.split("#");
     async function doWork() {
-        const tag = searchbox.value.split("#");
+      const tag = searchbox.value.split("#");
       try {
-        const response = await fetch(`${apiUrl}mmr/na/${tag[0]}/${tag[1]}`);
+        // for the ranking and name
+        const response = await fetch(`${apiUrl}mmr/na/${tag[0]}/${tag[1]}`); //fetch mmr na only
         const json = await response.json();
         console.log(json.data.name);
         console.log(json.data.ranking_in_tier);
-        rrtext.innerHTML = json.data.ranking_in_tier + "/100";
-        nametext.innerHTML = json.data.name;
-        const r2 = await fetch(`${apiUrl}account/${tag[0]}/${tag[1]}`);
+        rrtext.innerHTML = json.data.ranking_in_tier + "/100"; //mmr
+        nametext.innerHTML = json.data.name; //name
+        // for the banner and account level
+        const r2 = await fetch(`${apiUrl}account/${tag[0]}/${tag[1]}`); //fetch the account stats
         const rjson = await r2.json();
         console.log(rjson);
         console.log(rjson.data.card.large);
-        banner.style.background =  `url("${rjson.data.card.large}")`;
-      } catch {
-        
-      }
+        banner.style.background = `url("${rjson.data.card.large}")`; //banner
+        level.innerHTML = rjson.data.account_level; //acc level
+      } catch {}
     }
     doWork();
   });
