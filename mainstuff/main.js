392e3a5b69a1f4417acf73a10bd76b474a7f774d
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-06-13 18:44:31 -0400

    coded last mmr change and catch

--- mainstuff/main.js
+++ mainstuff/main.js
@@ -13,25 +13,41 @@ window.onload = function () {
     e.preventDefault();
     const tag = searchbox.value.split("#");
     async function doWork() {
+      //FUNCTION START                     FIRST IS MMR AND RANK THEN ITS NORMAL ACCOUNT
       const tag = searchbox.value.split("#");
       try {
-        // for the ranking and name
+        // RANKING AND NAME
         const response = await fetch(`${apiUrl}mmr/na/${tag[0]}/${tag[1]}`); //fetch mmr na only
-        const json = await response.json();
-        console.log(json.data.name);
-        console.log(json.data.ranking_in_tier);
-        rrtext.innerHTML = "RR = " + json.data.ranking_in_tier; //mmr
-        nametext.innerHTML = json.data.name; //name
-        let tier = json.data.currenttierpatched.replace(" ", "%20");
-        rank.innerHTML = `<img src="Val_icons/${tier}.png" alt="" class="rankpic">`;
+        const json = await response.json(); //convert the mmr stuff to json
+        if (json.data.mmr_change_to_last_game > 0)
+          //IF RR IS LOST
+          rrtext.innerHTML =
+            "RR = " +
+            json.data.ranking_in_tier +
+            `<i class="fa-solid fa-arrow-up"></i>`;
+        //arrow up
+        else if (json.data.mmr_change_to_last_game < 0)
+          //IF RR IS GAINED
+          rrtext.innerHTML =
+            "RR = " +
+            json.data.ranking_in_tier +
+            `<i class="fa-solid fa-arrow-down"></i>`;
+        //arrow down
+        else rrtext.innerHTML = "RR = " + json.data.ranking_in_tier; //NO RR LOST OR GAINED
+        nametext.innerHTML = json.data.name; //SET NAME
+        let tier = json.data.currenttierpatched.replace(" ", "%20"); //FOR RANK IMAGE
+        rank.innerHTML = `<img src="Val_icons/${tier}.png" alt="" class="rankpic">`; //FOR RANK IMAGE
         // for the banner and account level
         const r2 = await fetch(`${apiUrl}account/${tag[0]}/${tag[1]}`); //fetch the account stats
-        const rjson = await r2.json();
-        console.log(rjson);
-        console.log(rjson.data.card.large);
-        banner.style.background = `url("${rjson.data.card.large}")`; //banner
-        level.innerHTML = "Level = " + rjson.data.account_level; //acc level
-      } catch {}
+        const rjson = await r2.json(); //convert norm stats to json
+        banner.style.background = `url("${rjson.data.card.large}")`; //BANNER
+        level.innerHTML = "Level = " + rjson.data.account_level; //ACCOUNT LEVEL
+      } catch {
+        rank.innerHTML = `<img src="Val_icons/Iron 1.png" alt="" class="rankpic">`;
+        nametext.innerHTML = `Unknown`;
+        rrtext.innerHTML = `RR = 00`;
+        level.innerHTML = "999";
+      }
     }
     doWork();
   });
