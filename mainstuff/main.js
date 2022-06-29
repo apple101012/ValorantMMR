d4bf0ae27bde38fe3dacae951b90348acad1de9c
apple101012 <85631620+apple101012@users.noreply.github.com>
2022-06-29 14:47:04 -0400

    god tier code

--- mainstuff/main.js
+++ mainstuff/main.js
@@ -12,6 +12,7 @@ window.onload = function () {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const tag = searchbox.value.split("#");
+    //FOR THE FIRST FUNCTION
     async function doWork() {
       //FUNCTION START                     FIRST IS MMR AND RANK THEN ITS NORMAL ACCOUNT
       const tag = searchbox.value.split("#");
@@ -49,6 +50,57 @@ window.onload = function () {
         level.innerHTML = "999";
       }
     }
+    async function matchHistory() {
+      const response = await fetch(
+        `https://api.henrikdev.xyz/valorant/v3/matches/na/${tag[0]}/${tag[1]}?filter=competitive`
+      );
+      const json = await response.json();
+      let one = json.data[4].players.all_players.find((x) => x.name == tag[0]); // player stats for match
+      let two = json.data[3].players.all_players.find((x) => x.name == tag[0]);
+      let three = json.data[2].players.all_players.find(
+        (x) => x.name == tag[0]
+      );
+      let four = json.data[1].players.all_players.find((x) => x.name == tag[0]);
+      let five = json.data[0].players.all_players.find((x) => x.name == tag[0]);
+      let matches = [five, four, three, two, one];
+      let flex = document.querySelector(".flex-container");
+      let a = 0;
+      for (i of matches) {
+        let team = i.team;
+        team = team.toLowerCase();
+        let teamstats = json.data[a].teams[team];
+        console.log(teamstats);
+        a++;
+        flex.innerHTML += `<div class="flex-item">
+        <div class="character c1">
+          <img
+            src="${i.assets.agent.small}"
+            alt=""
+            class="character-pic"
+          />
+        </div>
+        <div class="kda text1">KDA <span class="kda1">${i.stats.kills}/${
+          i.stats.deaths
+        }/${i.stats.assists}</span></div>
+        <div class="winloss winloss1 text1">
+          ${teamstats.has_won ? "Victory" : "Defeat"}
+          <span class="score">${teamstats.rounds_won}/${
+          teamstats.rounds_lost
+        }</span>
+        </div>
+        <div class="acs text1">ACS <span class="acs1">200</span></div>
+
+        <div class="map">
+          <img
+            src="https://static.wikia.nocookie.net/valorant/images/1/13/Loading_Screen_Icebox.png"
+            alt=""
+          />
+        </div>
+      </div>`;
+      }
+      console.log(one);
+    }
     doWork();
+    matchHistory();
   });
 };
